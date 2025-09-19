eb2b98d1e4eca6210c6858b69c2dc72964d53bb6
Laimonas Mikalauskas <laimislietuvis@gmail.com>
2025-09-19 17:28:10 +0300

    Add files via upload

--- task 1.py
+++ task 1.py
@@ -1,22 +1,9 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request
+from flask_restful import Resource, Api
+from calendar import calendar
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///projects.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-app = Flask(__name__)
+api = Api(app)
 
 
-@app.route('/')
-def index():
-    return render_template
 
-
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
-
-__import__("style.css")
