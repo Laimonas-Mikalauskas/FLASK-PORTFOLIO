15a9c91eeedece71f8e85824b60c6d83ec1669f7
Laimonas Mikalauskas <laimislietuvis@gmail.com>
2025-10-06 20:14:54 +0300

    Add files via upload

---
+++ Mobile apps and software/1 app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def main_page():
+    return render_template("index.html")
+
+@app.route("/")
+def home():
+    return """
+    <name>"Hello World!!!"<name>
+    <h1>Homepage</h1>
+    <p>About Us</p>
+
+@app.route("/")
+def homepage():
+    (__import__("style.css")
+
+"))
+     
+if __name__ == "__main__":
+    app.run()"""
+
+
+
+
+
